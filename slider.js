3488f65678e67ed105130ac4261b7ea9c1f83e36
K3nK0 <uncommonmen@hotmail.fr>
2023-08-03 23:37:35 +0200

    section my work HTML and start slider

---
+++ slider.js
@@ -0,0 +1,11 @@
+// gestion slider
+
+$('.slider').slick({
+    centerMode: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    variableWidth: true,
+    accessibility: true
+  });
